51f52acf7eaf2a0c590830097ba04c4563988280
Mr Jaynath <141899550+sunil143786@users.noreply.github.com>
2024-08-03 13:35:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,7 +22,7 @@ USERNAME = environ.get('USERNAME', "https://telegram.me/Mr_Jaybath")
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002073485610'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/Jaynath_Request_Group')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002055127272').split()]
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Jkbot:jkbot@cluster0.gict7fb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Jkbot:jkbot@cluster0.gict7fb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "TELEGRAM_BOT_INFO")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002073485610'))
